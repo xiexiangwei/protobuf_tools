489841bfc195ec97eda69507c6561295a48acb7d
xiexiangwei <xxw19900622@qq.com>
2017-05-14 21:49:20 +0800

    脚本更新

--- CreatePb.bat
+++ CreatePb.bat
@@ -13,6 +13,12 @@ set Python_COMPILER_PATH=.\PythonTools\protoc.exe
 ::Python_TARGET_PATH文件生成路径, 最后不要跟“\”符号
 set Python_TARGET_PATH=.
 
+::C#协议文件所在位置
+set CS_PROTOCOL_PATH=D:\U3DPro\xGameClient\Assets\_Scripts
+::Python协议文件所在位置
+set PYTHON_PROTOCOL_PATH=D:\xGame\common
+
+
 ::删除之前创建的文件
 del %CS_TARGET_PATH%\*.cs /f /s /q
 del %Python_TARGET_PATH%\*.py /f /s /q
@@ -32,5 +38,12 @@ for /f "delims=" %%i in ('dir /b "%SOURCE_FOLDER%\*.proto"') do (
 )
 
 echo 协议生成完毕。
+
+::替换旧的文件
+echo xcopy %CS_TARGET_PATH%\CmdMessage.cs  %CS_PROTOCOL_PATH%\CmdMessage.cs /s /e /y
+xcopy %CS_TARGET_PATH%\CmdMessage.cs  %CS_PROTOCOL_PATH%\CmdMessage.cs /s /e /y
+echo xcopy %Python_TARGET_PATH%\CmdMessage_pb2.py %PYTHON_PROTOCOL_PATH%\CmdMessage_pb2.py /s /e /y
+xcopy %Python_TARGET_PATH%\CmdMessage_pb2.py %PYTHON_PROTOCOL_PATH%\CmdMessage_pb2.py /s /e /y
+
 
 pause
